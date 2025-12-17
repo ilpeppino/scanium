c1bddfbbf4ee35405d81c5e05581df6edd3679a4
Giuseppe Tempone <ilpeppino@gmail.com>
2025-12-17 13:24:00 +0100

    build: add KMP skeleton for shared/core-models

---
+++ shared/core-models/build.gradle.kts
@@ -0,0 +1,76 @@
+plugins {
+    kotlin("multiplatform")
+    kotlin("plugin.serialization") version "1.9.20"
+}
+
+kotlin {
+    // Android target
+    androidTarget()
+
+    // iOS targets
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
+
+    sourceSets {
+        // Common source sets
+        val commonMain by getting {
+            dependencies {
+                // Kotlinx Serialization for data models
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
+            }
+        }
+
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
+
+        // Android source sets
+        val androidMain by getting {
+            dependencies {
+                // Android-specific dependencies if needed
+            }
+        }
+
+        val androidUnitTest by getting {
+            dependencies {
+                implementation(kotlin("test-junit"))
+            }
+        }
+
+        // iOS source sets
+        val iosMain by creating {
+            dependsOn(commonMain)
+        }
+
+        val iosTest by creating {
+            dependsOn(commonTest)
+        }
+
+        // Configure individual iOS targets to use shared iosMain/iosTest
+        val iosX64Main by getting { dependsOn(iosMain) }
+        val iosArm64Main by getting { dependsOn(iosMain) }
+        val iosSimulatorArm64Main by getting { dependsOn(iosMain) }
+
+        val iosX64Test by getting { dependsOn(iosTest) }
+        val iosArm64Test by getting { dependsOn(iosTest) }
+        val iosSimulatorArm64Test by getting { dependsOn(iosTest) }
+    }
+}
+
+// Android configuration
+android {
+    namespace = "com.scanium.core.models"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 24
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
